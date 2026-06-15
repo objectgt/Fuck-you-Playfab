cf7ed1af4a4ffa7af0c739b407dd735a2810514a
unknown <averypaxton5716@Gmail.com>
2026-06-14 23:40:02 -0400

    initial source upload

---
+++ Fuck you Playfab/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fuck_you_Playfab
+{
+    public class Constants
+    {
+        public const string GUID = "org.gorillatag.objectgt.fuckyouplayfab";
+        public const string Name = "Fuck you Playfab";
+        public const string Version = "0.0.1";
+    }
+}
---
+++ Fuck you Playfab/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using GorillaNetworking;
+using HarmonyLib;
+
+namespace Fuck_you_Playfab
+{
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Harmony harmony = new Harmony(Constants.GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayFabAuthenticator), "Awake")]
+    class incinerateplayfab
+    {
+        [HarmonyPrefix]
+        public static bool patchplayfabauthenticator()
+        {
+            return false;
+        }
+    }
+}
